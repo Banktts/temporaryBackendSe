e33b09712b8eb1a57e0c0c52ad49c56bba1c9ea7
Banktts <bankthamasaroch@gmail.com>
2021-02-11 02:20:32 +0700

    fix: dir path not match issue

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,7 @@
 docker-compose down
-docker volume rm backend_BackendDb
+path=`pwd`
+dirname=`basename $path | awk '{print tolower($0)}'`
+docker volume rm ${dirname}_BackendDb
 docker-compose up -d
 sleep 20
 sh dbBuild.sh
