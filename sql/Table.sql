03c82a6f92802dff452d97f8c08a83ae7164edf7
Banktts <bankthamasaroch@gmail.com>
2021-02-10 19:36:40 +0700

    change to main table file

--- sql/Table.sql
+++ sql/Table.sql
@@ -13,7 +13,7 @@ CREATE TABLE restaurant (
 CREATE TABLE menu (
 	M_ID int NOT NULL PRIMARY KEY,
 	R_ID int NOT NULL,
-	M_name varchar(20)NOT NULL,
+	M_name varchar(25)NOT NULL,
 	M_price int NOT NULL,
 	M_type varchar(6) NOT NULL,
 	M_image_url varchar(255) NOT NULL
@@ -23,9 +23,6 @@ CREATE TABLE customer (
 	C_ID int   NOT NULL PRIMARY KEY,
 	C_latitude  FLOAT(6) NOT NULL,
 	C_longitude  FLOAT(6) NOT NULL
-
 );
-
 ALTER TABLE menu
 ADD FOREIGN KEY (R_ID) REFERENCES restaurant(R_ID);
-
