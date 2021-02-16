eeea5ba60f36c276c64f9fdbb2c58793d66b6af9
Thanabodee Thamasaroch <bankthamasaroch@gmail.com>
2021-02-17 02:30:41 +0700

    fix: remove part of create go mod

--- dbBuild.sh
+++ dbBuild.sh
@@ -1,9 +1,6 @@
 echo "db build"
 go get github.com/go-sql-driver/mysql
 go get go.mongodb.org/mongo-driver
-go clean -modcache  
-go mod init example.com 
-go mod tidy
 echo "go build succeed"
 go run sql/setupSql.go
 go run mongo/setupMongo.go
