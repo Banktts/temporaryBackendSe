1f7333cd9b11800e028a2dec0063a7500683a97a
Thanabodee Thamasaroch <bankthamasaroch@gmail.com>
2021-02-17 02:50:47 +0700

    add default homepage

--- main.go
+++ main.go
@@ -2,9 +2,25 @@ package main
 
 import(	
 	Api "example.com/temporaryBackendSe/api"
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+	"log"
 	
 )
 
+func  homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"Homepage")
+}
+
+func handleRequests(){
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/",homePage)
+	log.Fatal(http.ListenAndServe(":9000",myRouter))
+}
+
+
 func main(){
+	handleRequests()
 	Api.Test()
 }
