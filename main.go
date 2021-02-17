34d0b4b2a6e9969be8fa811360687f89af577f71
paliita <amppalita0905@gmail.com>
2021-02-18 00:42:15 +0700

    create res struct

--- main.go
+++ main.go
@@ -7,12 +7,22 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func allMenu(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(Api.AllMenu())
 }
 
+func getRestaurant(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	i, err := strconv.Atoi(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+	json.NewEncoder(w).Encode(Api.GetRestaurant(i))
+}
 
 func  homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w,"Homepage")
@@ -22,11 +32,11 @@ func handleRequests(){
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/",homePage)
 	myRouter.HandleFunc("/allmenu", allMenu).Methods("GET")
+	myRouter.HandleFunc("/restaurant/{id}", getRestaurant).Methods("GET")
 	log.Fatal(http.ListenAndServe(":9000",myRouter))
 }
 
 
 func main(){
 	handleRequests()
-
 }
