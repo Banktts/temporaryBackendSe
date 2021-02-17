359f5c23eb71a5589c82c7bd5bdfa0e9a09f0cdb
Thanabodee Thamasaroch <bankthamasaroch@gmail.com>
2021-02-17 18:08:01 +0700

    feat all menu

---
+++ api/allMenu.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"context"
+)
+
+
+
+func AllMenu() {
+	ExtraMenu:=extraMenu(17)
+	fmt.Println(ExtraMenu)
+}
+
+func extraMenu(mid int) []bson.M {
+	fmt.Println("allMenu")
+	extra := connectMongoDB().Collection("EXTRA_ITEM")
+	var extras []bson.M
+	rawData,err:=extra.Find(context.TODO(),bson.M{"M_id": mid})
+	if err != nil {
+		fmt.Println(err)
+	}
+	rawData.All(context.TODO(),&extras)
+	disconectMongoDB()
+	return extras
+}
