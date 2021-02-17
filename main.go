dfa10b8e14b536692c267cb7bbcdce6c48b2c30a
paliita <amppalita0905@gmail.com>
2021-02-18 02:37:56 +0700

    fix sql banner

--- main.go
+++ main.go
@@ -11,17 +11,22 @@ import (
 )
 
 func allMenu(w http.ResponseWriter, r *http.Request){
-	json.NewEncoder(w).Encode(Api.AllMenu())
+	json.NewEncoder(w).Encode(Api.AllMenu(17))
 }
 
-func getRestaurant(w http.ResponseWriter, r *http.Request){
+func getRestaurantBanner(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	i, err := strconv.Atoi(params["id"])
-	if err != nil {
-		panic(err.Error())
+
+	lat, err1 := strconv.ParseFloat(params["latitude"], 64)
+	if err1 != nil {
+		panic(err1.Error())
 	}
-	json.NewEncoder(w).Encode(Api.GetRestaurant(i))
+	long, err2 := strconv.ParseFloat(params["longitude"], 64)
+	if err2 != nil {
+		panic(err2.Error())
+	}
+	json.NewEncoder(w).Encode(Api.GetRestaurantBanner(params["keyword"], lat, long))
 }
 
 func  homePage(w http.ResponseWriter, r *http.Request){
@@ -32,11 +37,12 @@ func handleRequests(){
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/",homePage)
 	myRouter.HandleFunc("/allmenu", allMenu).Methods("GET")
-	myRouter.HandleFunc("/restaurant/{id}", getRestaurant).Methods("GET")
+	myRouter.HandleFunc("/restaurant/{keyword}/{latitude}/{longitude}", getRestaurantBanner).Methods("GET")
 	log.Fatal(http.ListenAndServe(":9000",myRouter))
 }
 
 
 func main(){
 	handleRequests()
+
 }
