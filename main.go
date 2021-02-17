24b915d74ffe7445f7dd8082a4c49359c4635279
paliita <amppalita0905@gmail.com>
2021-02-18 03:36:53 +0700

    add getBanner

--- main.go
+++ main.go
@@ -29,6 +29,23 @@ func getRestaurant(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(Api.GetRestaurant(params["keyword"], lat, long))
 }
 
+func getBanner(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	lat, err1 := strconv.ParseFloat(params["latitude"], 64)
+	if err1 != nil {
+		panic(err1.Error())
+	}
+	long, err2 := strconv.ParseFloat(params["longitude"], 64)
+	if err2 != nil {
+		panic(err2.Error())
+	}
+
+	fmt.Println(lat, long)
+	json.NewEncoder(w).Encode(Api.GetBanner(lat, long))
+}
+
 func  homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w,"Homepage")
 }
@@ -38,11 +55,11 @@ func handleRequests(){
 	myRouter.HandleFunc("/",homePage)
 	myRouter.HandleFunc("/allmenu", allMenu).Methods("GET")
 	myRouter.HandleFunc("/restaurant/{keyword}/{latitude}/{longitude}", getRestaurant).Methods("GET")
+	myRouter.HandleFunc("/banner/{latitude}/{longitude}", getBanner).Methods("GET")
 	log.Fatal(http.ListenAndServe(":9000",myRouter))
 }
 
 
 func main(){
 	handleRequests()
-
 }
