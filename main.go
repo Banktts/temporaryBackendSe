c3eda7b055619c17bc182fb2cc0cfb6a93c49c35
Thanabodee Thamasaroch <bankthamasaroch@gmail.com>
2021-02-17 20:58:11 +0700

    test api all menu

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	Api "example.com/temporaryBackendSe/api"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
+
+func allMenu(w http.ResponseWriter, r *http.Request){
+	json.NewEncoder(w).Encode(Api.AllMenu())
+}
+
 
 func  homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w,"Homepage")
@@ -15,12 +21,12 @@ func  homePage(w http.ResponseWriter, r *http.Request){
 func handleRequests(){
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/",homePage)
+	myRouter.HandleFunc("/allmenu", allMenu).Methods("GET")
 	log.Fatal(http.ListenAndServe(":9000",myRouter))
 }
 
 
 func main(){
-	Api.AllMenu()
 	handleRequests()
 
 }
