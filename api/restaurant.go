0fe2e0d99800420f11e0dbef51f52476f90515bd
paliita <amppalita0905@gmail.com>
2021-02-22 01:59:48 +0700

    add R_location

--- api/restaurant.go
+++ api/restaurant.go
@@ -11,13 +11,14 @@ type Res struct {
 	R_isRecomend bool `json:"R_isRecomend"`
 	R_image_url string `json:"R_image_url"` 
 	R_distance float64 `json:"R_distance"`
+	R_location string `json:"R_location"`
   }
 
 
 func GetRestaurant(keyword string, latitude float64, longitude float64)[]Res {	
 	db := connectSqlDB()
 
-	rows, err:= db.Query("select R_ID, R_name, R_rating, R_votes, R_isRecomend, R_image_url, (sqrt(power((R_latitude-?),2)+power((R_longitude-?),2)))*100 as R_distance from restaurant where R_name like '%"+keyword+"%' order by R_distance", latitude, longitude)
+	rows, err:= db.Query("select R_ID, R_name, R_rating, R_votes, R_isRecomend, R_image_url, (sqrt(power((R_latitude-?),2)+power((R_longitude-?),2)))*100 as R_distance, R_location from restaurant where R_name like '%"+keyword+"%' order by R_distance", latitude, longitude)
 	if err!=nil{
 		fmt.Println(err)
 	}
@@ -27,7 +28,7 @@ func GetRestaurant(keyword string, latitude float64, longitude float64)[]Res {
 	var restaurantList []Res
 	for rows.Next() {
 		var res Res
-		err := rows.Scan(&res.R_ID, &res.R_name, &res.R_rating, &res.R_votes, &res.R_isRecomend, &res.R_image_url, &res.R_distance)
+		err := rows.Scan(&res.R_ID, &res.R_name, &res.R_rating, &res.R_votes, &res.R_isRecomend, &res.R_image_url, &res.R_distance, &res.R_location)
 		if err != nil {
 			panic(err.Error())
 		}
