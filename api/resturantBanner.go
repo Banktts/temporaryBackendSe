442966ecc112e633a70c9d226e5eaf455264b252
paliita <amppalita0905@gmail.com>
2021-02-22 02:18:00 +0700

    add R_location for abnner

--- api/resturantBanner.go
+++ api/resturantBanner.go
@@ -7,7 +7,7 @@ import(
 func GetBanner(latitude float64, longitude float64)[]Res {	
 	db := connectSqlDB()
 
-	rows, err:= db.Query("select R_ID, R_name, R_rating, R_votes, R_isRecomend, R_image_url, (sqrt(power((R_latitude-?),2)+power((R_longitude-?),2)))*100 as R_distance from restaurant where R_isRecomend = 1 order by R_distance limit 5" , latitude, longitude)
+	rows, err:= db.Query("select R_ID, R_name, R_rating, R_votes, R_isRecomend, R_image_url, (sqrt(power((R_latitude-?),2)+power((R_longitude-?),2)))*100 as R_distance, R_location from restaurant where R_isRecomend = 1 order by R_distance limit 5" , latitude, longitude)
 	if err!=nil{
 		fmt.Println(err)
 	}
@@ -17,7 +17,7 @@ func GetBanner(latitude float64, longitude float64)[]Res {
 	var bannerList []Res
 	for rows.Next() {
 		var res Res
-		err := rows.Scan(&res.R_ID, &res.R_name, &res.R_rating, &res.R_votes, &res.R_isRecomend, &res.R_image_url, &res.R_distance)
+		err := rows.Scan(&res.R_ID, &res.R_name, &res.R_rating, &res.R_votes, &res.R_isRecomend, &res.R_image_url, &res.R_distance, &res.R_location)
 		if err != nil {
 			panic(err.Error())
 		}
