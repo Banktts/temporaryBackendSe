54e89581113f2fef6260400ee9766c07c98568ea
sarasix <60171924+sarasix@users.noreply.github.com>
2021-03-27 10:59:43 +0700

    Update status.go

--- api/status.go
+++ api/status.go
@@ -9,7 +9,7 @@ type Deliveryman struct {
 	D_name string `json:"D_name"`
 	D_rating float64 `json:"D_rating"`
 	D_tel string `json:"D_phone"`
-	Waiting_time string `json:"Waiting_time"`
+	Waiting_time float64 `json:"Waiting_time"`
   }
 
 func OrderInfo(DeliveryId int, OrderId int)Deliveryman{
@@ -20,10 +20,42 @@ func OrderInfo(DeliveryId int, OrderId int)Deliveryman{
 	if err!=nil{
 		fmt.Println(err)
 	}
-	d.Waiting_time = "123"
+	d.Waiting_time = WaitingTime(DeliveryId,OrderId)
 	return d
 }
+func WaitingTime(D_ID int,O_ID int) float64{
+    db := connectSqlDB()
+    var D_latitude float64
+    var D_longitude float64
+    var C_latitude float64
+    var C_longitude float64
 
+    rows, err:= db.Query("select D_latitude,D_longitude from delivery_man where D_ID = ? ",D_ID)
+    if err!=nil{
+        fmt.Println(err)
+    }
+    for rows.Next() {
+        
+        err := rows.Scan(&D_latitude,&D_longitude)
+        if err != nil {
+            panic(err.Error())
+        }
+        
+    }
+    rows2, err2:= db.Query("select customer.C_latitude,customer.C_longitude from ordert natural join customer where O_ID = ? ",O_ID)
+    if err2!=nil{
+        fmt.Println(err2)
+    }
+    for rows2.Next() {
+        
+        err2 := rows2.Scan(&C_latitude,&C_longitude)
+        if err2 != nil {
+            panic(err2.Error())
+        }
+        
+    }
+    return 20+ math.Pow(math.Pow(D_latitude-C_latitude,2) + math.Pow(D_longitude-C_longitude,2),1/2)
+}
 
  
 
